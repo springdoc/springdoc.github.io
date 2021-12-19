2fb8b60cabfbaf9af362f7a35375aec8ff4fa544
bnasslah <badr.nasslahsen@gmail.com>
2021-12-20 00:40:15 +0100

    Template update

--- docs/js/toc.js
+++ docs/js/toc.js
@@ -12,16 +12,16 @@ if (toctitle != null) {
         headingSelector: 'h1, h2, h3, h4, h5',
         positionFixedSelector: 'body',
         fixedSidebarOffset: 90,
-        smoothScroll: false
+        scrollSmooth: false
     });
     if (!path.endsWith("index.html") && !path.endsWith("/")) {
         var link = document.createElement("a");
         if (document.getElementById('index-link')) {
-          indexLinkElement = document.querySelector('#index-link > p > a');
-          linkHref = indexLinkElement.getAttribute("href");
-          link.setAttribute("href", linkHref);
+            indexLinkElement = document.querySelector('#index-link > p > a');
+            linkHref = indexLinkElement.getAttribute("href");
+            link.setAttribute("href", linkHref);
         } else {
-          link.setAttribute("href", "index.html");
+            link.setAttribute("href", "index.html");
         }
         link.innerHTML = "<span><i class=\"fa fa-chevron-left\" aria-hidden=\"true\"></i></span> Back to index";
         var block = document.createElement("div");
@@ -43,9 +43,3 @@ var headerHtml = '<div id="header-spring">\n' +
 var header = document.createElement("div");
 header.innerHTML = headerHtml;
 document.body.insertBefore(header, document.body.firstChild);
-
-
-if (window.matchMedia('screen and (max-width: 768px)').matches) {
-    document.getElementById("toc").remove();
-    document.body.className="book";
-}
